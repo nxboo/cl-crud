6f7ac04890ee6330d61af6c37d68003dcb18a5c4
cool-team-official <cool_team@163.com>
2020-05-22 14:13:13 +0800

    优化

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const resolve = (dir) => path.resolve(__dirname, dir);
 
 const webpackConfig = {
-    devtool: 'source-mapx',
+    devtool: 'source-map',
     mode: 'production',
     entry: './src/index.js',
     output: {
